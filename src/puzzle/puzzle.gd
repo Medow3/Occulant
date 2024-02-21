b6bb55a7a2a60ee6ca1904f9e9a562b1cf767318
grace <graceschorno@protonmail.com>
2024-02-21 12:39:31 -0800

    Add puzzle node

---
+++ src/puzzle/puzzle.gd
@@ -0,0 +1,14 @@
+class_name Puzzle extends TileMap
+
+@export var reticle = Vector2i(5, 5)
+
+@export var dimensions = Vector2i(19, 11)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ src/puzzle/puzzle_global.gd
@@ -0,0 +1,3 @@
+extends Node
+
+signal puzzle_finished(p: Puzzle);
